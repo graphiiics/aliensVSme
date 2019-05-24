eb52f3efa5a3bf60c42016c6a8d0e15bf30c28ec
graphiiics <gaelgeovanimq@gmail.com>
2019-05-24 15:12:39 -0500

    the sky and ground is ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+//import PropTypes from 'prop-types'
+import Canvas from './components/Canvas'
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>this is in building...</h1>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <Canvas />
+      // <div className="App">
+      //   <h1>{this.props.message}</h1>
+      // </div>
+    );
+  }
 }
+
+// App.propTypes = {
+//   message: PropTypes.string.isRequired,
+// }
 
 export default App;
